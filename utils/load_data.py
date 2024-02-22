bebea68edc452896f2be0c282f97ba7f75b7ddb6
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-02-22 16:30:46 +0800

    Create load_data.py

---
+++ utils/load_data.py
@@ -0,0 +1,18 @@
+def load_data(dataset_dir, num_episodes, camera_names, batch_size_train, batch_size_val):
+    print(f'\nData from: {dataset_dir}\n')
+    # obtain train test split
+    train_ratio = 0.8
+    shuffled_indices = np.random.permutation(num_episodes)
+    train_indices = shuffled_indices[:int(train_ratio * num_episodes)]
+    val_indices = shuffled_indices[int(train_ratio * num_episodes):]
+
+    # obtain normalization stats for qpos and action
+    norm_stats = get_norm_stats(dataset_dir, num_episodes)
+
+    # construct dataset and dataloader
+    train_dataset = EpisodicDataset(train_indices, dataset_dir, camera_names, norm_stats)
+    val_dataset = EpisodicDataset(val_indices, dataset_dir, camera_names, norm_stats)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, pin_memory=True, num_workers=1, prefetch_factor=1)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size_val, shuffle=True, pin_memory=True, num_workers=1, prefetch_factor=1)
+
+    return train_dataloader, val_dataloader, norm_stats, train_dataset.is_sim

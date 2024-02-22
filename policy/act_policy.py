9c19dfc430704d6f9a0eeb2704ff6331a4be0064
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-02-22 19:51:18 +0800

    Update act_policy.py

--- policy/act_policy.py
+++ policy/act_policy.py
@@ -3,7 +3,9 @@ from torch.nn import functional as F
 import torchvision.transforms as transforms
 import torch
 import numpy as np
+
 from models import build_ACT_model_and_optimizer
+
 import IPython
 e = IPython.embed
 
@@ -14,7 +16,7 @@ class ACTPolicy(nn.Module):
         model, optimizer = build_ACT_model_and_optimizer(args)
         self.model = model
         self.optimizer = optimizer
-        self.kl_weight = args['kl_weight']
+        self.kl_weight = args.kl_weight
         
 
     def __call__(self, qpos, image, actions=None, is_pad=None, vq_sample=None):
@@ -26,12 +28,8 @@ class ACTPolicy(nn.Module):
         
         ### Training
         if actions is not None:
-            actions = actions[:, :self.model.num_queries]
-            is_pad = is_pad[:, :self.model.num_queries]
-            
             a_hat, is_pad_hat, (mu, logvar), probs, binaries = self.model(qpos, image, None, actions, is_pad, vq_sample)
 
-            loss_dict = dict()
             all_l1 = F.l1_loss(actions, a_hat, reduction='none')
             l1 = (all_l1 * ~is_pad.unsqueeze(-1)).mean()
             total_kld, dim_wise_kld, mean_kld = kl_divergence(mu, logvar)
@@ -41,7 +39,7 @@ class ACTPolicy(nn.Module):
 
         ### Inference
         else:
-            a_hat, _, (_, _), _, _ = self.model(qpos, image, None, vq_sample=vq_sample) # no action, sample from prior
+            a_hat, _, (_, _), _, _ = self.model(qpos, image, None, vq_sample=vq_sample)
             
             return a_hat
 

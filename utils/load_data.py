562fa290ab5c0f804314b44b55eab1ad9152377f
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-02-22 16:56:38 +0800

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,38 +1,85 @@
 import os
+import h5py
 import torch
 import numpy as np
-from glob import glob
-from torch.utils.data import Dataset
-from pytorch3d.ops import knn_points, knn_gather
+from torch.utils.data import Dataset, DataLoader
 
-from .acronym import load_mesh, load_grasps
+from .normalize import get_norm_stats
 
+import IPython
+e = IPython.embed
 
-class Dataset(Dataset):
-    
-    
-    def __init__(self, args):
-        super().__init__()
-        
-        self.object_fname = glob(os.path.join(object_dir, mode, "**.h5"))
-        self.mesh_dir = os.path.join(mesh_dir, mode)
-        self.point_num = point_num
-    
-    
-    def __getitem__(self, key):
-        T, success = load_grasps(self.object_fname[key])
-        mesh = load_mesh(self.object_fname[key], mesh_root_dir=self.mesh_dir)
-        point_cloud = mesh.sample(self.point_num)
-        point_cloud = torch.from_numpy(point_cloud).float()
-        return point_cloud, T, success
-    
-    
+class EpisodicDataset(Dataset):
+    def __init__(self, episode_ids, dataset_dir, camera_names, norm_stats):
+        super(EpisodicDataset).__init__()
+        self.episode_ids = episode_ids
+        self.dataset_dir = dataset_dir
+        self.camera_names = camera_names
+        self.norm_stats = norm_stats
+        self.is_sim = None
+        self.__getitem__(0) # initialize self.is_sim
+
     def __len__(self):
-        return len(self.object_fname)
+        return len(self.episode_ids)
+
+    def __getitem__(self, index):
+        sample_full_episode = False # hardcode
+
+        episode_id = self.episode_ids[index]
+        dataset_path = os.path.join(self.dataset_dir, f'episode_{episode_id}.hdf5')
+        with h5py.File(dataset_path, 'r') as root:
+            is_sim = root.attrs['sim']
+            original_action_shape = root['/action'].shape
+            episode_len = original_action_shape[0]
+            if sample_full_episode:
+                start_ts = 0
+            else:
+                start_ts = np.random.choice(episode_len)
+            # get observation at start_ts only
+            qpos = root['/observations/qpos'][start_ts]
+            qvel = root['/observations/qvel'][start_ts]
+            image_dict = dict()
+            for cam_name in self.camera_names:
+                image_dict[cam_name] = root[f'/observations/images/{cam_name}'][start_ts]
+            # get all actions after and including start_ts
+            if is_sim:
+                action = root['/action'][start_ts:]
+                action_len = episode_len - start_ts
+            else:
+                action = root['/action'][max(0, start_ts - 1):] # hack, to make timesteps more aligned
+                action_len = episode_len - max(0, start_ts - 1) # hack, to make timesteps more aligned
+
+        self.is_sim = is_sim
+        padded_action = np.zeros(original_action_shape, dtype=np.float32)
+        padded_action[:action_len] = action
+        is_pad = np.zeros(episode_len)
+        is_pad[action_len:] = 1
+
+        # new axis for different cameras
+        all_cam_images = []
+        for cam_name in self.camera_names:
+            all_cam_images.append(image_dict[cam_name])
+        all_cam_images = np.stack(all_cam_images, axis=0)
+
+        # construct observations
+        image_data = torch.from_numpy(all_cam_images)
+        qpos_data = torch.from_numpy(qpos).float()
+        action_data = torch.from_numpy(padded_action).float()
+        is_pad = torch.from_numpy(is_pad).bool()
+
+        # channel last
+        image_data = torch.einsum('k h w c -> k c h w', image_data)
+
+        # normalize image and change dtype to float
+        image_data = image_data / 255.0
+        action_data = (action_data - self.norm_stats["action_mean"]) / self.norm_stats["action_std"]
+        qpos_data = (qpos_data - self.norm_stats["qpos_mean"]) / self.norm_stats["qpos_std"]
+
+        return image_data, qpos_data, action_data, is_pad
 
 
 def load_data(dataset_dir, num_episodes, camera_names, batch_size_train, batch_size_val):
-    print(f'\nData from: {dataset_dir}\n')
+    
     # obtain train test split
     train_ratio = 0.8
     shuffled_indices = np.random.permutation(num_episodes)
@@ -42,10 +89,8 @@ def load_data(dataset_dir, num_episodes, camera_names, batch_size_train, batch_s
     # obtain normalization stats for qpos and action
     norm_stats = get_norm_stats(dataset_dir, num_episodes)
 
-    # construct dataset and dataloader
-    train_dataset = EpisodicDataset(train_indices, dataset_dir, camera_names, norm_stats)
-    val_dataset = EpisodicDataset(val_indices, dataset_dir, camera_names, norm_stats)
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True, pin_memory=True, num_workers=1, prefetch_factor=1)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size_val, shuffle=True, pin_memory=True, num_workers=1, prefetch_factor=1)
-
-    return train_dataloader, val_dataloader, norm_stats, train_dataset.is_sim
+    # Construct dataset and dataloader
+    dataset = EpisodicDataset(train_indices, dataset_dir, camera_names, norm_stats)
+    dataloader = DataLoader(dataset, batch_size=batch_size_train, shuffle=True, pin_memory=True, num_workers=1, prefetch_factor=1)
+   
+    return dataloader, norm_stats
